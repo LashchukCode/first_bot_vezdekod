362e4eec356b8019b74e9328573d8741b515aa94
LashchukCode <102252640+LashchukCode@users.noreply.github.com>
2022-04-10 03:58:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Тут находятся токен и id-группы
-TOKEN = "77d8498ab724de73aadaacc336e27a18de3b199e139656a06347eeb71d5af86280054dd756626fc7339af"
+TOKEN = "Скину, если нужен"
 PUBLIC_ID = "212546973"
 
 # https://vk.com/public212546973
